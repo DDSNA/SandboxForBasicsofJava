154d4ceb15ccb4f333bad8989d038f13a59aefdc
DDSNA <79444147+DDSNA@users.noreply.github.com>
2023-02-05 16:03:38 +0200

    added it to the constructor oops, I think I just had my first developer-copyright "duh" moment

--- src/main/java/syntax/demo/main.java
+++ src/main/java/syntax/demo/main.java
@@ -8,7 +8,7 @@ class item_inventoryobjectTemplate {
     float price;
 
     //constructor (needs to include all of the above)
-    item_inventoryobjectTemplate(String type, String quantity_type, float price) {
+    item_inventoryobjectTemplate(String type, String quantity_type, double quantity, float price) {
         this.type = type;
         this.quantity_type = quantity_type;
         this.quantity = quantity;
@@ -23,13 +23,14 @@ class item_inventoryobjectTemplate {
 
     double updatestockQuantity(double updatedstockQuantity) {
         this.quantity = updatedstockQuantity;
-        return  this.quantity;
+        return this.quantity;
     }
 
     //objects
 
-    item_inventoryobjectTemplate faina = new item_inventoryobjectTemplate("aliment de baza", "buc", 15f);
-    item_inventoryobjectTemplate cascaval = new item_inventoryobjectTemplate("aliment de baza","kg", 4.11f);
+    item_inventoryobjectTemplate faina = new item_inventoryobjectTemplate("aliment de baza", "bucata",12,15f);
+    item_inventoryobjectTemplate salam = new item_inventoryobjectTemplate("mezeluri","bucata",37,13.5f);
+    item_inventoryobjectTemplate cascaval = new item_inventoryobjectTemplate("aliment de baza","kilogram",12, 4.11f);
 }
 
 public class main {
