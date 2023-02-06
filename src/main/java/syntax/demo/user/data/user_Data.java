5f36853e3221014af718e7a5b5586a5300b5d928
DDSNA <79444147+DDSNA@users.noreply.github.com>
2023-02-06 06:46:12 +0200

    hmm... constructors don't work this way afterall...

---
+++ src/main/java/syntax/demo/user/data/user_Data.java
@@ -0,0 +1,18 @@
+package syntax.demo.user.data;
+
+public class user_Data {
+    private String userName;
+    private String passwordUnescure;
+    public String userType;
+
+
+    public user_Data(String userName, String passwordUnescure, String userType) {
+        this.userType = userType;
+        this.userName = userName;
+        this.passwordUnescure = passwordUnescure;
+    }
+
+    private new_user_Data(String userName, String passwordUnescure, String userType) {
+        this.userType = ;
+    }
+}
