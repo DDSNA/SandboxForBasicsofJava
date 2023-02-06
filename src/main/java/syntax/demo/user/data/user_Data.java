22a8a1807ebd928fd2418c6ff88a876644c53639
DDSNA <79444147+DDSNA@users.noreply.github.com>
2023-02-06 08:35:37 +0200

    created a new object, constructor and methods for it for practice

--- src/main/java/syntax/demo/user/data/user_Data.java
+++ src/main/java/syntax/demo/user/data/user_Data.java
@@ -1,18 +1,25 @@
 package syntax.demo.user.data;
 
+//class
 public class user_Data {
     private String userName;
     private String passwordUnescure;
     public String userType;
 
-
+//constructor
     public user_Data(String userName, String passwordUnescure, String userType) {
         this.userType = userType;
         this.userName = userName;
         this.passwordUnescure = passwordUnescure;
     }
 
-    private new_user_Data(String userName, String passwordUnescure, String userType) {
-        this.userType = ;
+    //method
+    public String update_userType(String updated_userType) {
+        this.userType = updated_userType;
+        return this.userType;
     }
+
+    //objects
+
+    user_Data Dan = new user_Data("Dan","09876","admin");
 }
