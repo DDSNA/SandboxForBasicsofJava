0a0e26464c0e87f564f6a6849a8c50b8a600c193
DDSNA <79444147+DDSNA@users.noreply.github.com>
2023-02-10 16:15:37 +0200

    Multithreading attempt

--- src/main/java/syntax/demo/loopsandfun/MultithreadingExample.java
+++ src/main/java/syntax/demo/loopsandfun/MultithreadingExample.java
@@ -9,6 +9,9 @@ public class MultithreadingExample extends Thread {
     public void run(){
         for (int x = 1; x <= 20; x++) {
             System.out.println(x + "from thread number: " + ThreadNumber);
+            if (ThreadNumber == 3) {
+                throw new RuntimeException("Funkytime!!!!!! You got a three so it's a party in the threads! Also thread 3 stopped working");
+            }
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
