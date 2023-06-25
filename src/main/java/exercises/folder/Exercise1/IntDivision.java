6c4673a773e2c6b265a0967e096d54759dba0e8d
Dan <79444147+DDSNA@users.noreply.github.com>
2023-06-25 15:06:06 +0300

    intdivision exercise

--- src/main/java/exercises/folder/Exercise1/IntDivision.java
+++ src/main/java/exercises/folder/Exercise1/IntDivision.java
@@ -2,7 +2,10 @@ package exercises.folder.Exercise1;
 
 public class IntDivision {
     public static void main(String[] args){
+        int number_1 = 50;
+        int number_2 = 3;
 
+        System.out.println(number_1/number_2);
     }
 
 }
