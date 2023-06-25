cbd36dc81cc625f4be27a419d52f4014c07fa0a0
Dan <79444147+DDSNA@users.noreply.github.com>
2023-06-25 15:04:38 +0300

    intsumprinting exercise

---
+++ src/main/java/exercises/folder/Exercise1/IntDivision.java
@@ -0,0 +1,8 @@
+package exercises.folder.Exercise1;
+
+public class IntDivision {
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/main/java/exercises/folder/Exercise1/IntSumPrinting.java
@@ -0,0 +1,11 @@
+package exercises.folder.Exercise1;
+
+public class IntSumPrinting {
+    public static void main(String[] args) {
+        int number_1 = 74;
+        int number_2 = 36;
+        int sum = number_1 + number_2;
+
+        System.out.println(sum);
+    }
+}
