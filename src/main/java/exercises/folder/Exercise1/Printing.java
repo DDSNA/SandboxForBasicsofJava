d56e5a846ab7ee8ba201f256478379bce7b6b437
Dan <79444147+DDSNA@users.noreply.github.com>
2023-06-25 15:01:06 +0300

    string exercise

---
+++ src/main/java/exercises/folder/Exercise1/Printing.java
@@ -0,0 +1,8 @@
+package exercises.folder.Exercise1;
+
+public class Printing {
+    public static void main(String[] args) {
+        String name = "Dan Serban";
+        System.out.printf("Hello %n%s", name);
+    }
+}
